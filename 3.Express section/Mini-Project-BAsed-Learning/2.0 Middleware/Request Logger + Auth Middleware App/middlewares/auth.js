97e48f165c63143973fecec79777908c40b2032c
rajat <rajat290@users.noreply.github.com>
2025-04-18 23:11:53 +0530

    Initialize middleware files and task description for Express app

---
+++ 3.Express section/Mini-Project-BAsed-Learning/2.0 Middleware/Request Logger + Auth Middleware App/middlewares/auth.js
@@ -0,0 +1,20 @@
+const express = require ('express');
+const logger = require ('../middlewares/logger');
+const auth = express.Router();
+
+const app = express();
+
+app.use (logger);
+
+//public route 
+
+app.get('./home',(req, res) => {
+    res.send ('Welcome to the home page');
+})
+
+//private route
+app.get ('./dashboard', (req, res) => {
+    res.send ('Welcome to the dashboard pagge(Protected page)');
+})
+
+//auth middleware
